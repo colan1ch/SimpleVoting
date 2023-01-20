d67e62ab2c2c6c7e9910494a35101f8ed926b4bb
Andrey Bakov <andrey.bakov.2006@gmail.com>
2023-01-20 23:39:04 +0300

    add base.html

--- simple_votings/settings.py
+++ simple_votings/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Далее полученное значение подставляется в соответствующую переменную
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'Insert secret key here and uncomment this variable'
+ SECRET_KEY = 'django-insecure-kn&0#@=kpd7@y##re%dl=wfbnfk280=d&0-e!p1#8(1uoro*%e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
