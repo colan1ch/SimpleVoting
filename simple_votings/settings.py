80ea91ee6c80e59e5285c0dedd67d95db7257f1f
Arseniy Sidorin <you@example.com>
2023-01-22 12:46:11 +0300

    loginhtml

--- simple_votings/settings.py
+++ simple_votings/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Далее полученное значение подставляется в соответствующую переменную
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'Insert secret key here and uncomment this variable'
+SECRET_KEY = '@q)v!$xpil9(sj-$s)7)munw)*bjt8r7bj8j=8*4%t+#)s^yoi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
