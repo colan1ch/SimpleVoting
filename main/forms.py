e686d9f398b9ea3d398b995502e2bd9f8a8bd291
Andrey Bakov <andrey.bakov.2006@gmail.com>
2023-02-03 23:11:40 +0300

    some design changes

--- main/forms.py
+++ main/forms.py
@@ -2,5 +2,5 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 class LoginPage(AuthenticationForm):
-    username = forms.CharField(label='Login', widget=forms.TextInput(attrs={'class':'form-control'}))
-    password = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class':'form-control'}))
+    username = forms.CharField(label='Login', widget=forms.TextInput(attrs={'placeholder':'Enter your login'}))
+    password = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'placeholder':'Enter your password'}))
