1d89ef691e10f9a733fc3481373b15fea088791a
Fedor__Zorikov <fedor.z4321@yandex.ru>
2023-02-04 13:27:12 +0300

    Вроде все должно работать, но проблема с юзером, мне ЕА в классе помог, но дома опять эта ошибка

--- main/models.py
+++ main/models.py
@@ -7,8 +7,7 @@ class UserSettings(models.Model):
 
 
 class Voting(models.Model):
-    title = models.CharField(max_length=100)
-    text = models.CharField(max_length=300)
-    type = models.SmallIntegerField()  # вид голосования 1 - radiobutton 2 - checkbox
-    options = models.TextField()  # json массив вариантов ответа
-    user = models.ForeignKey(to=User, on_delete=models.SET_NULL, null=True)
+     text = models.CharField(max_length=300)
+     type = models.SmallIntegerField()  # вид голосования 1 - radiobutton 2 - checkbox
+     options = models.TextField()  # json массив вариантов ответа
+     user = models.ForeignKey(to=User, on_delete=models.SET_NULL, null=True)
